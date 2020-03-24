90204a5345de94916bd6e73fc829f3038479d23d
Noah Oliver Giron Rigonan <oliverrigonan@gmail.com>
2020-03-24 09:52:23 +0800

    Add project files.

---
+++ CebuCovid2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CebuCovid2019.Startup))]
+namespace CebuCovid2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
